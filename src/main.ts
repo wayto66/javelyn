8482890e35ab1af867777328e3785ba5d0718b99
wayto66 <yurigarciadev@gmail.com>
2024-04-08 12:16:06 -0300

    https fix

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as fs from "fs";
 import { AppModule } from "./app.module";
 import { AllExceptionFilter } from "./infra/common/filter/all.exception.filter";
 import { LoggerService } from "./infra/common/logger/logger.service";
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: fs.readFileSync(
-  //     "../../../etc/letsencrypt/live/javelyn.link/privkey.pem",
-  //   ),
-  //   cert: fs.readFileSync(
-  //     "../../../etc/letsencrypt/live/javelyn.link/fullchain.pem",
-  //   ),
-  // };
-  // const app = await NestFactory.create(AppModule, { httpsOptions });
-
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(
+      "../../../etc/letsencrypt/live/javelyn.link/privkey.pem",
+    ),
+    cert: fs.readFileSync(
+      "../../../etc/letsencrypt/live/javelyn.link/fullchain.pem",
+    ),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   process.on("uncaughtException", (err) => {
     console.error("\x1b[31m%s\x1b[0m", "UNCAUGHT EXCEPTION!");
