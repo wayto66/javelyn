d56e5cfba8a412b38c2b04d5d871c8a71875c4e7
wayto66 <yurigarciadev@gmail.com>
2024-06-22 10:44:26 -0300

    ead-li webhook

--- src/webhooks/webhooks.module.ts
+++ src/webhooks/webhooks.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { PrismaService } from "prisma/prisma.service";
 import { WebhooksController } from "./webhooks.controller";
 import { WebhooksService } from "./webhooks.service";
 
 @Module({
   controllers: [WebhooksController],
   providers: [WebhooksService],
+  exports: [WebhooksService],
+  imports: [PrismaService],
 })
 export class WebhooksModule {}
