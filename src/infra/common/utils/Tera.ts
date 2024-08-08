c73b165f7be3619ff8891a84e4fd0eb46e6fd331
wayto66 <yurigarciadev@gmail.com>
2024-08-08 14:52:35 -0300

    notify tera hmac signature

--- src/infra/common/utils/Tera.ts
+++ src/infra/common/utils/Tera.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import crypto from "crypto";
 
 export interface INotifyParams {
   body: any;
@@ -8,7 +9,17 @@ export interface INotifyParams {
 export class Tera {
   static async notify({ body, endpoint }: INotifyParams) {
     const teraUrl = process.env.TERA_URL;
-    const response = await axios.post(`${teraUrl}/notify/${endpoint}`, body);
+    const secret = process.env.HMAC_SECRET;
+    const signature = crypto
+      .createHmac("sha256", secret)
+      .update(JSON.stringify(body))
+      .digest("hex");
+
+    const response = await axios.post(`${teraUrl}/notify/${endpoint}`, body, {
+      headers: {
+        "x-mac-signature": signature,
+      },
+    });
     const data = await response.data;
     return data;
   }
